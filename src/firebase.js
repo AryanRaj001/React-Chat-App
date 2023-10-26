4557907ccd08a5ddc5ed58dbda4af8d8dc83924c
Aryan Raj <rajaryan.msd28@gmail.com>
2023-10-26 19:54:08 +0530

    Project Added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAz0TED4fg2vl_JW2WiPKkIOkcH1W86h8",
+  authDomain: "aryan--chatapp.firebaseapp.com",
+  projectId: "aryan--chatapp",
+  storageBucket: "aryan--chatapp.appspot.com",
+  messagingSenderId: "67961572487",
+  appId: "1:67961572487:web:c910218725c87341dc7c22"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
